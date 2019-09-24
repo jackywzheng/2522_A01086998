47cf8cf2b25e05609a9df0d9e2cc4005335c811f
Jacky Zheng <jackywzheng@gmail.com>
2019-09-24 16:55:19 -0700

    Started Pool class.

---
+++ src/ca/bcit/comp2522/assignments/A1/Pool.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.assignments.A1;
+
+public class Pool {
+    public static final String DEFAULT_POOL_NAME = "Unnamed";
+    public static final double DEFAULT_POOL_TEMP_CELSIUS = 40.0;
+    public static final double MINIMUM_POOL_TEMP_CELSIUS = 0.0;
+    public static final double MAXIMUM_POOL_TEMP_CELSIUS = 100.0;
+    public static final double NEUTRAL_PH = 7.0;
+    public static final double DEFAULT_NUTRIENT_COEFFICIENT = 0.50;
+    public static final double MINIMUM_NUTRIENT_COEFFICIENT = 0.0;
+    public static final double MAXIMUM_NUTRIENT_COEFFICIENT = 1.0;
+
+
+}
